2de1142ce57f5ec52f9f5c639131d50a9416b80c
Haruki Oshiro <harukioshiro875421@gmail.com>
2025-10-14 00:10:52 +0900

    delete all

--- examples/best_mm.rs
+++ examples/best_mm.rs
@@ -8,32 +8,22 @@ use bitbankutil_rs::depth::Depth;
 use crypto_botters::generic_api_client::websocket::WebSocketConfig;
 use log::LevelFilter;
 use rust_decimal::prelude::*;
-struct MyBot {
-    pair: String,
-    tick_size: Decimal,
-    refresh_cycle: u128,
-    lot: Decimal,
-    max_lot: Decimal,
-    bb_api_client: BitbankPrivateApiClient,
-    state: MyBotState,
-}
 
-struct MyBotState {
+struct MyBot {
+    bot_config: MyBotConfig,
     depth: BitbankDepth,
     last_updated: u128,
     last_bestbid: Decimal,
     last_bestask: Decimal,
 }
 
-impl MyBotState {
-    fn new() -> MyBotState {
-        MyBotState {
-            depth: BitbankDepth::new(),
-            last_updated: 0,
-            last_bestbid: Decimal::zero(),
-            last_bestask: Decimal::zero(),
-        }
-    }
+struct MyBotConfig {
+    pair: String,
+    tick_size: Decimal,
+    refresh_cycle: u128,
+    lot: Decimal,
+    max_lot: Decimal,
+    bb_api_client: BitbankPrivateApiClient,
 }
 
 impl MyBot {
@@ -47,13 +37,18 @@ impl MyBot {
         max_lot: Decimal,
     ) -> MyBot {
         MyBot {
-            pair: pair,
-            tick_size,
-            refresh_cycle,
-            lot,
-            max_lot,
-            bb_api_client: BitbankPrivateApiClient::new(bitbank_key, bitbank_secret, None),
-            state: MyBotState::new(),
+            bot_config: MyBotConfig {
+                pair,
+                tick_size,
+                refresh_cycle,
+                lot,
+                max_lot,
+                bb_api_client: BitbankPrivateApiClient::new(bitbank_key, bitbank_secret, None),
+            },
+            depth: BitbankDepth::new(),
+            last_updated: 0,
+            last_bestbid: Decimal::zero(),
+            last_bestask: Decimal::zero(),
         }
     }
 
@@ -64,35 +59,35 @@ impl MyBot {
             .unwrap()
             .as_millis();
 
-        assert!(self.state.last_updated <= now);
+        assert!(self.last_updated <= now);
 
-        if now - self.state.last_updated >= self.refresh_cycle {
+        if now - self.last_updated >= self.bot_config.refresh_cycle {
             log::info!(
                 "{} milliseconds have passed since the last order update",
-                now - self.state.last_updated
+                now - self.last_updated
             );
 
-            if !self.state.depth.is_complete() {
+            if !self.depth.is_complete() {
                 log::info!("depth is not complete");
                 return;
             }
 
             // update `self.last_updated` here, in order to prevent call api too frequently
             // mutable reference needed here.
-            self.state.last_updated = SystemTime::now()
+            self.last_updated = SystemTime::now()
                 .duration_since(UNIX_EPOCH)
                 .unwrap()
                 .as_millis();
 
-            let bb_client2 = self.bb_api_client.clone();
-            let pair2 = self.pair.clone();
+            let bb_client2 = self.bot_config.bb_api_client.clone();
+            let pair2 = self.bot_config.pair.clone();
             let order_info_task = tokio::spawn(async move {
                 bb_client2
                     .get_active_orders(Some(&pair2), None, None, None, None, None)
                     .await
             });
 
-            let bb_client3 = self.bb_api_client.clone();
+            let bb_client3 = self.bot_config.bb_api_client.clone();
             let current_asset_task = tokio::spawn(async move { bb_client3.get_assets().await });
 
             // wait until two task has ended
@@ -118,7 +113,7 @@ impl MyBot {
 
             log::debug!("active orders: {:?}", active_orders_info);
 
-            let asset_name = self.pair.split("_").next().unwrap();
+            let asset_name = self.bot_config.pair.split("_").next().unwrap();
 
             let btc_asset = current_asset
                 .assets
@@ -148,20 +143,21 @@ impl MyBot {
             let btc_free_amount = btc_asset.free_amount.clone().parse::<Decimal>().unwrap();
             let btc_locked_amount = btc_asset.locked_amount.clone().parse::<Decimal>().unwrap();
             let btc_amount = btc_free_amount + btc_locked_amount;
-            let btc_amount_remainder = btc_amount - (btc_amount / self.lot).floor() * self.lot;
+            let btc_amount_remainder =
+                btc_amount - (btc_amount / self.bot_config.lot).floor() * self.bot_config.lot;
             let jpy_free_amount = jpy_asset.free_amount.clone().parse::<Decimal>().unwrap();
             let jpy_amount = jpy_free_amount + btc_locked_jpy_amount;
 
             log::debug!("btc_free_amount: {:?}, btc_locked_amount: {:?}, jpy_free_amount{:?}, btc_locked_jpy_amount: {:?}", btc_free_amount, btc_locked_amount, jpy_free_amount, btc_locked_jpy_amount);
             log::info!(
                 "{}_amount: {}, jpy_amount: {}",
-                self.pair.clone(),
+                self.bot_config.pair.clone(),
                 btc_amount,
                 jpy_amount
             );
 
-            let best_ask_price = self.state.depth.best_ask().unwrap().0.clone();
-            let best_bid_price = self.state.depth.best_bid().unwrap().0.clone();
+            let best_ask_price = self.depth.best_ask().unwrap().0.clone();
+            let best_bid_price = self.depth.best_bid().unwrap().0.clone();
 
             let has_bestask_order = active_orders_info.clone().orders.iter().any(|ord| {
                 ord.side == "sell"
@@ -176,43 +172,45 @@ impl MyBot {
             });
 
             let sell_price = {
-                if has_bestask_order || best_ask_price - self.tick_size == best_bid_price {
+                if has_bestask_order || best_ask_price - self.bot_config.tick_size == best_bid_price
+                {
                     best_ask_price
                 } else {
-                    best_ask_price - self.tick_size
+                    best_ask_price - self.bot_config.tick_size
                 }
             };
 
             let buy_price = {
-                if has_bestbid_order || best_bid_price + self.tick_size == best_ask_price {
+                if has_bestbid_order || best_bid_price + self.bot_config.tick_size == best_ask_price
+                {
                     best_bid_price
                 } else {
-                    best_bid_price + self.tick_size
+                    best_bid_price + self.bot_config.tick_size
                 }
             };
 
             log::info!("target spread: {}", sell_price - buy_price);
 
-            let can_buy =
-                jpy_amount >= buy_price * self.lot && btc_amount + self.lot <= self.max_lot;
-            let can_sell = btc_amount >= self.lot;
+            let can_buy = jpy_amount >= buy_price * self.bot_config.lot
+                && btc_amount + self.bot_config.lot <= self.bot_config.max_lot;
+            let can_sell = btc_amount >= self.bot_config.lot;
 
             let mut wanna_place_orders = Vec::new();
 
             if can_buy {
                 wanna_place_orders.push(bitbankutil_rs::order_manager::SimplifiedOrder {
-                    pair: self.pair.clone(),
+                    pair: self.bot_config.pair.clone(),
                     side: "buy".to_owned(),
-                    amount: self.lot,
+                    amount: self.bot_config.lot,
                     price: buy_price,
                 });
             }
 
             if can_sell {
                 wanna_place_orders.push(bitbankutil_rs::order_manager::SimplifiedOrder {
-                    pair: self.pair.clone(),
+                    pair: self.bot_config.pair.clone(),
                     side: "sell".to_owned(),
-                    amount: self.lot + btc_amount_remainder,
+                    amount: self.bot_config.lot + btc_amount_remainder,
                     price: sell_price,
                 });
             }
@@ -220,13 +218,13 @@ impl MyBot {
             log::debug!("wanna_place_orders: {:?}", wanna_place_orders);
             log::info!("evaluated asset: {}", btc_amount * sell_price + jpy_amount);
             {
-                let bb_client = self.bb_api_client.clone();
+                let bb_client = self.bot_config.bb_api_client.clone();
                 bitbankutil_rs::order_manager::place_wanna_orders_concurrent(
                     wanna_place_orders,
                     active_orders_info.orders,
                     btc_free_amount,
                     jpy_free_amount,
-                    self.pair.clone(),
+                    self.bot_config.pair.clone(),
                     bb_client,
                 )
                 .await;
@@ -254,19 +252,19 @@ impl BotStrategy for MyBot {
                     let bestask = depth.best_ask().unwrap().0.clone();
                     let bestbid = depth.best_bid().unwrap().0.clone();
 
-                    if bestask != self.state.last_bestask || bestbid != self.state.last_bestbid {
+                    if bestask != self.last_bestask || bestbid != self.last_bestbid {
                         log::debug!(
                             "best ask diff: {}, best bid diff: {}",
-                            bestask - self.state.last_bestask,
-                            bestbid - self.state.last_bestbid
+                            bestask - self.last_bestask,
+                            bestbid - self.last_bestbid
                         );
-                        self.state.last_bestask = bestask;
-                        self.state.last_bestbid = bestbid;
+                        self.last_bestask = bestask;
+                        self.last_bestbid = bestbid;
                     }
                 }
 
-                self.state.depth = depth;
-                if self.state.depth.is_complete() {
+                self.depth = depth;
+                if self.depth.is_complete() {
                     self.update_orders().await;
                 }
             }
